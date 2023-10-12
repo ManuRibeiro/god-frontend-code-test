5ef73f9b7a13bfd6cbe17bff7fe1db4757d20d60
ManuRibeiro <e.ribeiro@ua.pt>
2023-10-13 00:37:57 +0100

    fix:corsv1

--- pages/api/cars.ts
+++ pages/api/cars.ts
@@ -1,8 +1,10 @@
 import { NextApiRequest,NextApiResponse } from "next";
 import data from '../../public/api/cars.json';
+import { NextResponse } from "next/server";
 
-export default async function handler(req:NextApiRequest,res:NextApiResponse){
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.setHeader("Access-Control-Allow-Credentials","true");
-    res.status(200).json(data);
+export async function GET(req:NextApiRequest,res:NextApiResponse){
+    
+    res.setHeader('Access-Control-Allow-Origin', `${req.headers.origin}`)
+    res.status(200).json(data)
+    
 }
