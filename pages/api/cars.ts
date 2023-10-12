bda8f8af92687aac153bf681b4238c097e1ae4fc
ManuRibeiro <e.ribeiro@ua.pt>
2023-10-12 05:58:49 +0100

    feat: add headers to api

--- pages/api/cars.ts
+++ pages/api/cars.ts
@@ -2,5 +2,6 @@ import { NextApiRequest,NextApiResponse } from "next";
 import data from '../../public/api/cars.json';
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse){
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.status(200).json(data);
 }
