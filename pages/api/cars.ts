c614cb25515937fd12afbd664d408537e572876f
ManuRibeiro <e.ribeiro@ua.pt>
2023-10-12 06:47:04 +0100

    fix:cors

--- pages/api/cars.ts
+++ pages/api/cars.ts
@@ -2,7 +2,7 @@ import { NextApiRequest,NextApiResponse } from "next";
 import data from '../../public/api/cars.json';
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse){
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    req.headers["access-control-allow-origin"]
+    res.setHeader("Access-Control-Allow-Origin","https://volvo-frontend-challenge-p5kfoatp4-emanuel-ribeiros-projects.vercel.app");
+    res.setHeader("Access-Control-Allow-Credentials","true");
     res.status(200).json(data);
 }
