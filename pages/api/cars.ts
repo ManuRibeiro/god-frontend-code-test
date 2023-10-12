dc27f02febfc556f83a77bccdec697b7f758f2d7
ManuRibeiro <e.ribeiro@ua.pt>
2023-10-12 06:11:48 +0100

    fix:cors

--- pages/api/cars.ts
+++ pages/api/cars.ts
@@ -3,5 +3,6 @@ import data from '../../public/api/cars.json';
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse){
     res.setHeader("Access-Control-Allow-Origin", "*")
+    req.headers["access-control-allow-origin"]
     res.status(200).json(data);
 }
