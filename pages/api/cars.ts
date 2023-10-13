665709ecb48c599c1bdab33722e5d98164776d2c
ManuRibeiro <e.ribeiro@ua.pt>
2023-10-13 01:21:05 +0100

    fix:set headers

--- pages/api/cars.ts
+++ pages/api/cars.ts
@@ -4,7 +4,10 @@ import data from '../../public/api/cars.json';
 
 export async function GET(req:NextApiRequest,res:NextApiResponse){
     
-    //res.setHeader('Access-Control-Allow-Origin', `${req.headers.origin}`)
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Credentials','true')
+    res.setHeader('Access-Control-Allow-Methods','GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader('Access-Control-Allow-Headers','X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version,Access-Control-Allow-Origin' )
     res.status(200).json(data)
     
 }
