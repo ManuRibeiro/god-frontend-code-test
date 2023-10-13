909274c942e043615e540b9fe720b18f8598f0e6
ManuRibeiro <e.ribeiro@ua.pt>
2023-10-13 01:11:31 +0100

    fix:

--- pages/api/cars.ts
+++ pages/api/cars.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest,NextApiResponse } from "next";
 import data from '../../public/api/cars.json';
-import { NextResponse } from "next/server";
+
 
 export async function GET(req:NextApiRequest,res:NextApiResponse){
     
-    res.setHeader('Access-Control-Allow-Origin', `${req.headers.origin}`)
+    //res.setHeader('Access-Control-Allow-Origin', `${req.headers.origin}`)
     res.status(200).json(data)
     
 }
--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -7,7 +7,8 @@ export function useCars(){
 
     useEffect(()=>{
         axios.get(process.env.NEXT_PUBLIC_API_URL+`/api/cars`,{headers:{
-            'Access-Control-Allow-Origin':'*'
+            'Access-Control-Allow-Origin':'*',
+            
         }})
         .then(res => {
             setCars(res.data);
