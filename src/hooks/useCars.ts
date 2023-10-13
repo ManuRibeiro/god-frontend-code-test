b007ea9afa189985dec539a975cd3cc5ed118678
ManuRibeiro <e.ribeiro@ua.pt>
2023-10-13 01:42:44 +0100

    fix:

--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -6,7 +6,10 @@ export function useCars(){
     const [cars,setCars] = useState<Car[]>([]);
 
     useEffect(()=>{
-        axios.get(process.env.NEXT_PUBLIC_API_URL+`/api/cars`)
+        axios.get(process.env.NEXT_PUBLIC_API_URL+`/api/cars`,{headers:{
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+        }})
         .then(res => {
             setCars(res.data);
         })
